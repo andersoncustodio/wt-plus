af75fd889da142f779886fd97167b53030e3a14a
Anderson Custódio <contato@andersoncustodio.com>
2022-02-16 22:00:53 -0300

    Redis service name fix

--- wt_plus/core/redis.py
+++ wt_plus/core/redis.py
@@ -8,17 +8,17 @@ class Redis(object):
             os.system('sudo apt install -y redis-server')
 
     def start(self):
-        os.system('sudo systemctl start redis.service')
+        os.system('sudo systemctl start redis-server.service')
 
     def stop(self):
-        os.system('sudo systemctl stop redis.service')
+        os.system('sudo systemctl stop redis-server.service')
 
     def restart(self):
-        os.system('sudo systemctl restart redis.service')
+        os.system('sudo systemctl restart redis-server.service')
 
     def enable(self,):
-        os.system('sudo systemctl enable redis.service')
+        os.system('sudo systemctl enable redis-server.service')
 
     def disable(self):
-        os.system('sudo systemctl disable redis.service')
+        os.system('sudo systemctl disable redis-server.service')
 
